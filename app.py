751274ba8e9359c097c443b3b31df4b48fac117d
Eugene Evstafiev <36392751+chigwell@users.noreply.github.com>
2022-04-01 13:55:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    data = {
-        "Modules" : 15,
-        "Subject" : "Data Structures and Algorithms",
-    }
-    return jsonify(data)
+@app.route('/', methods = ['GET'])
+def ReturnJSON():
+    if(request.method == 'GET'):
+        data = {
+            "Modules" : 15,
+            "Subject" : "Data Structures and Algorithms",
+        }
+  
+        return jsonify(data)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
